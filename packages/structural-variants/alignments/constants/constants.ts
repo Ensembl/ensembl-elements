92b3bfbc8c43c228ab700375cae2bb008b018b1b
Andrey Azov <andrey@ebi.ac.uk>
2026-01-22 17:39:21 +0000

    Always display structural variants in the variants-alignments component (#20)
    Make sure that structural variants are always visible regardless of how zoomed out the view is

--- packages/structural-variants/alignments/constants/constants.ts
+++ packages/structural-variants/alignments/constants/constants.ts
@@ -3,6 +3,8 @@ export const ALIGNMENT_AREA_HEIGHT = 135;
 export const IMAGE_HEIGHT = ALIGNMENT_AREA_HEIGHT + 2 * RULER_HEIGHT;
 export const VARIANT_HEIGHT = 6;
 
+export const STRUCTURAL_VARIANT_LENGTH_CUTOFF = 50; // variant length (extent) at which it is considered a structural variant
+
 export const COLORS = {
   alignment: '#0099ff', // Ensembl blue
   deletion: '#d90000', // Ensembl red
--- packages/structural-variants/alignments/parts/variants.ts
+++ packages/structural-variants/alignments/parts/variants.ts
@@ -1,7 +1,12 @@
 import { svg } from 'lit';
 import type { ScaleLinear } from 'd3';
 
-import { RULER_HEIGHT, VARIANT_HEIGHT, COLORS } from '../constants/constants';
+import {
+  RULER_HEIGHT,
+  VARIANT_HEIGHT,
+  COLORS,
+  STRUCTURAL_VARIANT_LENGTH_CUTOFF
+} from '../constants/constants';
 
 import type { Variant } from '../types/variant';
 
@@ -34,19 +39,24 @@ const renderVariant = ({
   const genomicStart = variant.location.start;
   const genomicEnd = variant.location.end;
 
+  const isStructuralVariant = variant.extent >= STRUCTURAL_VARIANT_LENGTH_CUTOFF;
+
   const [viewportGenomicStart, viewportGenomicEnd] = scale.domain();
   const viewportGenomicDistance = viewportGenomicEnd - viewportGenomicStart;
+  const shouldShowAllVariants = viewportGenomicDistance <= 100_000;
   
   const x = scale(genomicStart);
   const y = RULER_HEIGHT;
   let width = scale(genomicEnd + 1) - scale(genomicStart);
   let color = COLORS[variant.type as keyof typeof COLORS] || '#000000';
 
-  if (!width) {
-    if (viewportGenomicDistance > 100_000) {
-      return null;
-    } else {
+  if (variant.extent < STRUCTURAL_VARIANT_LENGTH_CUTOFF && !shouldShowAllVariants) {
+    return null;
+  } else if (!width) {
+    if (isStructuralVariant || shouldShowAllVariants) {
       width = 1;
+    } else {
+      return null;
     }
   }
 
