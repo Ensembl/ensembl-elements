0142743461bae5b4090c3949a4a99fad32980ef5
Andrey Azov <andrey@ebi.ac.uk>
2026-02-16 16:44:15 +0000

    Extract min gene tracks count into a constant

--- packages/regulation/region-annotation/constants.ts
+++ packages/regulation/region-annotation/constants.ts
@@ -2,6 +2,7 @@ export const GENE_TRACKS_TOP_OFFSET = 40;
 export const GENE_HEIGHT = 8;
 export const GENE_LABEL_HEIGHT = 10;
 export const GENE_TRACK_HEIGHT = GENE_HEIGHT + GENE_LABEL_HEIGHT + 3;
+export const MIN_GENE_TRACKS_COUNT = 2; // reserve space for at least this many gene tracks (per strand)
 
 export const REGULATORY_FEATURE_TRACKS_TOP_OFFSET = 20;
 
